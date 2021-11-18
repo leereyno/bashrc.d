157387ee6d826f8da73b2a9ecf9abe8e38061f36
leereyno <leebreynolds@gmail.com>
2021-11-18 16:37:24 -0500

    Path fix attempt
    Signed-off-by: leereyno <leebreynolds@gmail.com>

--- install.sh
+++ install.sh
@@ -6,7 +6,14 @@ if [ ! -d ~/bashrc.d ] ; then
 fi
 
 # Copy over our new profile files
-/bin/cp -av bashrc.d/* ~/bashrc.d/
+
+# If we have a directory on the command line, then use it as source
+# directory for files
+if [ $# -ge 1 ] ; then
+	/bin/cp -av ${1}/bashrc.d/* ~/bashrc.d/
+else
+	/bin/cp -av bashrc.d/* ~/bashrc.d/
+fi
 
 # Add lines to ~/.bashrc to source our profile files
 if [ ! "$(grep BASHRCD ~/.bashrc)" ] ; then
